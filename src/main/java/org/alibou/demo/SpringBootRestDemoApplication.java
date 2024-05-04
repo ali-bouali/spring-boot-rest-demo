0dc69a63083d8318b3961548fb8f880aaad2c885
ali-bouali <bouali.pro@gmail.com>
2024-05-04 10:46:57 +0200

    init project

---
+++ src/main/java/org/alibou/demo/SpringBootRestDemoApplication.java
@@ -0,0 +1,13 @@
+package org.alibou.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alibou/demo/SpringBootRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alibou.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
