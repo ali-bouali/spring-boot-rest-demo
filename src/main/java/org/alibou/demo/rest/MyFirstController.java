25aca1c60a5124ca8549ec10f3ecac69079cbe22
ali-bouali <bouali.pro@gmail.com>
2024-05-05 10:02:45 +0200

    spring rest - 05.05.2024

--- src/main/java/org/alibou/demo/rest/MyFirstController.java
+++ src/main/java/org/alibou/demo/rest/MyFirstController.java
@@ -1,11 +1,16 @@
 package org.alibou.demo.rest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -17,7 +22,9 @@ import java.util.List;
 public class MyFirstController {
 
 
+    // http://localhost:8080/api/v1/dassd/student
     @PostMapping("/students")
+    //@RequestMapping(value = "/students", method = RequestMethod.POST)
     public ResponseEntity<Student> saveStudent(
             @RequestBody Student student
     ) {
@@ -30,7 +37,18 @@ public class MyFirstController {
         return ResponseEntity.created(studentLocation).body(student);
     }
 
-    @GetMapping("/students")
+    @PostMapping("/students/special")
+    public ResponseEntity<Student> saveSpecialStudent(
+            @RequestBody Student student,
+            @RequestHeader("X-SOURCE-FROM") String from
+    ) {
+        if (!"mobile-app".equals(from)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    //@GetMapping("/students")
     public ResponseEntity<Student> getStudent() {
         Student s = new Student();
         s.setFirstname("Ali");
@@ -38,9 +56,24 @@ public class MyFirstController {
         return ResponseEntity.ok(s);
     }
 
-    @GetMapping("/students/{id}")
+    // http://localhost:8080/api/v1/dassd/students/1
+    // POST http://localhost:8080/api/v1/dassd/students/1/courses/2
+    @GetMapping("/students/{id-student}/courses/{course-id}")
     public ResponseEntity<Student> getStudentById(
-            @PathVariable("id") Integer id
+            @PathVariable(name = "id-student") Integer idStudent
+    ) {
+        Student s = new Student();
+        s.setFirstname("Ali");
+        s.setLastname("Bouali");
+        return ResponseEntity.ok(s);
+    }
+
+    // http://localhost:8080/api/v1/dassd/students?id-student=1&id-course=2
+    // http://localhost:8080/api/v1/dassd/students
+    @GetMapping("/students")
+    public ResponseEntity<Student> getStudentById2(
+            @RequestParam(value = "id-student", required = false, defaultValue = "1") Integer idStudent,
+            @RequestParam(value = "id-course", required = false, defaultValue = "2") Integer idCourse
     ) {
         Student s = new Student();
         s.setFirstname("Ali");
@@ -55,5 +88,13 @@ public class MyFirstController {
         s.setFirstname("Ali");
         s.setLastname("Bouali");
         return ResponseEntity.ok(s);
+    }
+
+
+    @DeleteMapping
+    public ResponseEntity<?> delete() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("");
     }
 }
